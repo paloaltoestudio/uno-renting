55b57a6bdacbcab9c98da3b03c5f9b3e493b8ca7
David <david@paloaltoestudio.com>
2021-01-06 16:56:56 -0500

    UNO Static Files

---
+++ precache-manifest.e89c0821796b546e7db1ef9646a32681.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ec18704524c1b50b451530157a34f2",
+    "url": "/uno-renting/index.html"
+  },
+  {
+    "revision": "97ac64fe05f520cbff08",
+    "url": "/uno-renting/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "8d65f7b4fdf254e14e77",
+    "url": "/uno-renting/static/css/main.c3cda2f8.chunk.css"
+  },
+  {
+    "revision": "97ac64fe05f520cbff08",
+    "url": "/uno-renting/static/js/2.13c2b068.chunk.js"
+  },
+  {
+    "revision": "cf5ed0248078f78cc342c6402c70262c",
+    "url": "/uno-renting/static/js/2.13c2b068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d65f7b4fdf254e14e77",
+    "url": "/uno-renting/static/js/main.98500565.chunk.js"
+  },
+  {
+    "revision": "35c90247a99c7d121daa",
+    "url": "/uno-renting/static/js/runtime-main.e81dfd4a.js"
+  },
+  {
+    "revision": "ecf2305af9dd20e09ef54af1556e47e9",
+    "url": "/uno-renting/static/media/body_bg.ecf2305a.jpg"
+  },
+  {
+    "revision": "082efadfac2e260b88f479e9d19e0c79",
+    "url": "/uno-renting/static/media/dk_buntaro-webfont.082efadf.woff"
+  },
+  {
+    "revision": "4dfdb3572e2489d5d21a23bc9f564e91",
+    "url": "/uno-renting/static/media/dk_buntaro-webfont.4dfdb357.eot"
+  },
+  {
+    "revision": "6c16c0170e1b189c152ea323ffff41b8",
+    "url": "/uno-renting/static/media/dk_buntaro-webfont.6c16c017.woff2"
+  },
+  {
+    "revision": "774938fbcedd6228f10899d994689930",
+    "url": "/uno-renting/static/media/dk_buntaro-webfont.774938fb.ttf"
+  },
+  {
+    "revision": "0cebf1f2daaca52fd3c432553990c7ab",
+    "url": "/uno-renting/static/media/segway.0cebf1f2.png"
+  }
+]);
