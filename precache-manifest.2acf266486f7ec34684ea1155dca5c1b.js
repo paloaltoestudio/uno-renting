88104fe783a5720da6e827bd012d2c2300a4c5c6
David <david@paloaltoestudio.com>
2021-01-06 17:31:28 -0500

    static files

---
+++ precache-manifest.2acf266486f7ec34684ea1155dca5c1b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1d4e88130ca811ee89915d0789e0bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f93bab06f31f80e0669a",
+    "url": "/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "f21c26698c116c2257c9",
+    "url": "/static/css/main.8cc3c7fe.chunk.css"
+  },
+  {
+    "revision": "f93bab06f31f80e0669a",
+    "url": "/static/js/2.71adc21d.chunk.js"
+  },
+  {
+    "revision": "cf5ed0248078f78cc342c6402c70262c",
+    "url": "/static/js/2.71adc21d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21c26698c116c2257c9",
+    "url": "/static/js/main.091fdd61.chunk.js"
+  },
+  {
+    "revision": "59210c18984c6934755d",
+    "url": "/static/js/runtime-main.3e42ff72.js"
+  },
+  {
+    "revision": "ecf2305af9dd20e09ef54af1556e47e9",
+    "url": "/static/media/body_bg.ecf2305a.jpg"
+  },
+  {
+    "revision": "082efadfac2e260b88f479e9d19e0c79",
+    "url": "/static/media/dk_buntaro-webfont.082efadf.woff"
+  },
+  {
+    "revision": "4dfdb3572e2489d5d21a23bc9f564e91",
+    "url": "/static/media/dk_buntaro-webfont.4dfdb357.eot"
+  },
+  {
+    "revision": "6c16c0170e1b189c152ea323ffff41b8",
+    "url": "/static/media/dk_buntaro-webfont.6c16c017.woff2"
+  },
+  {
+    "revision": "774938fbcedd6228f10899d994689930",
+    "url": "/static/media/dk_buntaro-webfont.774938fb.ttf"
+  },
+  {
+    "revision": "0cebf1f2daaca52fd3c432553990c7ab",
+    "url": "/static/media/segway.0cebf1f2.png"
+  }
+]);
